8de6dc695dc2754576e68204e391f2cbdf2f3083
index325 <gabriel.moraes.o@hotmail.com>
2020-06-27 04:05:39 -0300

    upload de avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
+import multer from "multer";
+import uploadConfig from "../config/upload";
 import CreateUserService from "../services/CreateUserService";
+import ensureAuthenticated from "../middlewares/ensureAuthenticated";
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post("/", async (req, res) => {
   try {
@@ -22,5 +26,14 @@ usersRouter.post("/", async (req, res) => {
     return res.status(400).json({ error: error.message });
   }
 });
+
+usersRouter.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  async (req, res) => {
+    return res.json({ okay: true });
+  }
+);
 
 export default usersRouter;
